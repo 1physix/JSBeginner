2bd50ab1a0dfc2740274f1765cd3ce60860ba305
Physix <159613996+1physix@users.noreply.github.com>
2025-05-16 14:43:15 +0000

    del useless

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let age = prompt("How old are you?-> ");
+age = Number(age)
 let name = prompt("What is your name?-> ");
 
 if (age >= 18) {
-    console.log(`${name} is an adult`)
+    console.log(`${name} is an adult.`);
+} else if (age >= 13) {
+        console.log(`${name} is a teenager.`); 
+} else {
+    console.log("You are a child.");
 }
-    else if (age >= 13) {
-        console.log(`${name} is a teenager
-    }      
